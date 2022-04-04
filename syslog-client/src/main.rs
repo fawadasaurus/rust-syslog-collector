46371e931e62665684c75d9a68edd8ce92947dbd
Fawad Shaikh <fawad@vino.dev>
2022-04-04 12:56:53 -0400

    TCP and UDP server

--- syslog-client/src/main.rs
+++ syslog-client/src/main.rs
@@ -1,3 +1,21 @@
+extern crate syslog;
+#[macro_use]
+extern crate log;
+
+use syslog::{Facility, Formatter3164, BasicLogger};
+use log::{SetLoggerError, LevelFilter};
+
 fn main() {
-    println!("Hello, world!");
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: "syslog-client".into(),
+        pid: 0,
+    };
+
+    let logger = syslog::udp(formatter,"127.0.0.1:8123","127.0.0.1:8514").expect("could not connect to syslog");
+    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
+            .map(|()| log::set_max_level(LevelFilter::Info));
+
+    info!("hello world");
 }
--- syslog-server/src/main.rs
+++ syslog-server/src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+use std::net::{UdpSocket, TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
+fn handle_udp_client(mut socket: UdpSocket) {
+    let mut buf = [0; 4096];
+    loop {
+        let sock = socket.try_clone();
+        match socket.recv_from(&mut buf) {
+            Ok((amt, src)) => {
+                thread::spawn(move || {
+                    //println!("Handling connection from {}", &src);
+                    let buf = &mut buf[..amt];
+                    println!("{}", std::str::from_utf8(&buf).unwrap());
+                });
+            },
+            Err(err) => {
+                eprintln!("Err: {}", err);
+            }
+        }
+    }
+}
+
+fn handle_tcp_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            println!("{}", std::str::from_utf8(&data[0..size]).unwrap());
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn main() -> std::io::Result<()> {
+    thread::spawn(move|| {
+        let tcp_listener = TcpListener::bind("0.0.0.0:1468").unwrap();
+        // accept connections and process them, spawning a new thread for each one
+        println!("TCPServer listening on port 1468");
+        for stream in tcp_listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("New connection: {}", stream.peer_addr().unwrap());
+                    thread::spawn(move|| {
+                        // connection succeeded
+                        handle_tcp_client(stream)
+                    });
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                    /* connection failed */
+                }
+            }
+        }
+    });
+
+
+    let socket = UdpSocket::bind("127.0.0.1:8514")?;
+    println!("UDP listening on port 8514");
+    handle_udp_client(socket);
+    Ok(())
 }
